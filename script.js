79f8a373749597a1055e65fb2312cc8142c8ef46
BHUVANESHWARI V <118094379+BhuvanaVadivel@users.noreply.github.com>
2024-06-30 19:11:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+/*console.log("Hello World")
+//var,const,int
+var a=5;
+a="KEC";
+a=1.0;
+a=true;
+let b=7;
+console.log(a);
+console.log(b);
+
+var arr = ["apple", "banana", "orange"];
+var arr1 = new Array("animal", "bird");
+arr.push("lemon");
+arr.push("pineapple");
+arr.unshift("watermelon");
+arr.unshift("strawbeery");
+arr.pop();
+console.log(arr.indexOf("apple"));
+*/
+
+var a=10;
+var b=20;
+if(a>b){
+    console.log("larger");
+}
+else{
+    console.log("larger");
+}
+/*
+for(var i=0; i<arr.length; i++)
+    {
+        console.log(arr[i]);
+    }
+for(const item of arr)
+    {
+        console.log(item);
+    }
+
+*/
+
